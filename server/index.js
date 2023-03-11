bdd93b441437a52bc41abc7f08713f19f85aa8eb
traciingu <traciinguyen@hotmail.com>
2023-03-10 22:40:12 -0500

    Handle POST request

--- server/index.js
+++ server/index.js
@@ -18,6 +18,26 @@ const Shortlinks = mongoose.model('Shortlinks', new mongoose.Schema(
         toObject: { virtuals: true }
     }));
 
+router.post('/', async (req, res) => {
+    let { url, shortlink } = req.body;
+
+    if (!url) {
+        res.status(400).send("URL property does not exist");
+    }
+
+    const match = await Shortlinks.find({ shortlink });
+
+    if (match.length === 0) {
+        if (!shortlink) {
+            shortlink = crypto.createHash('md5').update(url).digest('hex').slice(0, 10);
+        }
+
+        res.status(201).json(await new Shortlinks({ url, shortlink }).save());
+    } else {
+        res.status(200).send("Shortlink already exists");
+    }
+});
+
 
 app.use(router);
 
